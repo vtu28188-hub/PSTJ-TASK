f660df6fb249c03e5757fb3ef04f12462098dd60
vtu28188-hub <vtu28188@veltech.edu.in>
2026-08-28 09:27:19 +0530

    Create Session 4-1.java

---
+++ Week5/Session 4-1.java
@@ -0,0 +1,39 @@
+import java.util.HashSet;
+
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        
+        HashSet<Character> set = new HashSet<>();
+        
+        int left = 0;
+        int maxLength = 0;
+        
+        for (int right = 0; right < s.length(); right++) {
+            
+            // Remove characters until there is no duplicate
+            while (set.contains(s.charAt(right))) {
+                set.remove(s.charAt(left));
+                left++;
+            }
+            
+            set.add(s.charAt(right));
+            
+            maxLength = Math.max(maxLength, right - left + 1);
+        }
+        
+        return maxLength;
+    }
+}
+
+
+
+Input
+s =
+"abcabcbb"
+Output
+3
+Expected
+3
+
+
+
