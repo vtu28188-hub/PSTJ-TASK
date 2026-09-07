61726d74bebaa488884883c29605bf7e1e4efc41
vtu28188-hub <vtu28188@veltech.edu.in>
2026-09-07 10:19:26 +0530

    Create session1-1.java

---
+++ week7/session1-1.java
@@ -0,0 +1,40 @@
+class ParkingSystem {
+    int big, medium, small;
+
+    public ParkingSystem(int big, int medium, int small) {
+        this.big = big;
+        this.medium = medium;
+        this.small = small;
+    }
+
+    public boolean addCar(int carType) {
+        if (carType == 1) {
+            if (big > 0) {
+                big--;
+                return true;
+            }
+        } else if (carType == 2) {
+            if (medium > 0) {
+                medium--;
+                return true;
+            }
+        } else if (carType == 3) {
+            if (small > 0) {
+                small--;
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
+
+
+
+Input
+["ParkingSystem","addCar","addCar","addCar","addCar"]
+[[1,1,0],[1],[2],[3],[1]]
+Output
+[null,true,true,false,false]
+Expected
+[null,true,true,false,false]
